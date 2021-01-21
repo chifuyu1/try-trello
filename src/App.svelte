965e1b94eb44d56ddae75d846ee50a00ad8ba70f
Chifuyu <chifuyu@nate.com>
2021-01-21 22:00:37 +0900

    try temp code

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
 import Header from "~/components/Header.svelte";
 import ListContainer from "~/components/ListContainer.svelte";
-
 Object.assign(document.body.style, {
   backgroundColor: "darkgray",
   backgroundImage: "url(/images/bg.jpg)",
